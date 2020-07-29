158a5123f3f2388b90211f1b506148a6e8c3d1b9
19h47 <jeremylevron@19h47.fr>
2020-07-29 13:35:38 +0200

    :sparkles: Begin Hero

--- src/fragments.js
+++ src/fragments.js
@@ -4,7 +4,7 @@ export const fragments = graphql`
 	fragment HeroImage on File {
 		childImageSharp {
 			fluid(maxWidth: 1440) {
-				...GatsbyImageSharpFluid_tracedSVG
+				...GatsbyImageSharpFluid
 			}
 		}
 	}
--- src/templates/FrontPage.js
+++ src/templates/FrontPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 import Layout from '@/components/layout';
 import Seo from '@/components/seo';
@@ -10,12 +11,12 @@ export const query = graphql`
 			title
 			content
 			customFields {
-				test
-			}
-			featuredImage {
-				node {
-					remoteFile {
-						...HeroImage
+				hero {
+					title
+					thumbnail {
+						localFile {
+							...HeroImage
+						}
 					}
 				}
 			}
@@ -24,10 +25,37 @@ export const query = graphql`
 `;
 
 export default ({ data }) => {
+	console.log(data);
 	const { page } = data;
+	const {
+		title,
+		customFields: { hero },
+	} = page;
+
 	return (
 		<Layout>
-			<Seo title={data.title} />
+			<Seo title={title} />
+
+			<div className="Hero">
+				<div className="Site-container">
+					<div className="Hero__body">
+						<div className="Hero__content">
+							<div className="row">
+								<div className="col-12 offset-1">
+									<h1
+										className="Hero__title"
+										dangerouslySetInnerHTML={{ __html: hero.title }}
+									/>
+								</div>
+							</div>
+						</div>
+
+						<div className="Hero__thumbnail">
+							<Img fluid={hero.thumbnail.localFile.childImageSharp.fluid} />
+						</div>
+					</div>
+				</div>
+			</div>
 			<h1>{page.title}</h1>
 		</Layout>
 	);
