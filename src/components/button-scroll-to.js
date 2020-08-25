fc73cf3454805ac015244898ebac780caf1a28bd
19h47 <jeremylevron@19h47.fr>
2020-08-25 09:51:45 +0200

    :bug: Fix build Audio

--- src/components/button-scroll-to.js
+++ src/components/button-scroll-to.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const useAudio = url => {
-	const [audio] = useState(new Audio(url));
+	const [audio] = useState(new window.Audio(url));
 	const [playing, setPlaying] = useState(false);
 
 	const toggle = () => setPlaying(!playing);
