1e3de0ed7af20ef86cc39bf41b37b0547b81d494
19h47 <jeremylevron@19h47.fr>
2020-10-19 18:02:45 +0200

    :sparkles: Animate ContactBox
    close #15

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,5 +1,7 @@
 import React, { useContext, useRef, useEffect } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import { gsap } from 'gsap';
+import { CustomEase } from 'gsap/dist/CustomEase';
 
 import Socials from '@/components/socials';
 
@@ -10,8 +12,16 @@ import useInputEvent from '@/hooks/use-input-event';
 
 import Times from '@/assets/svg/times.inline.svg';
 
+gsap.registerPlugin(CustomEase);
+
 function Contacts() {
 	const contactsRef = useRef(null);
+	const titleRef = useRef(null);
+	const phoneRef = useRef(null);
+	const addressRef = useRef(null);
+	const mailRef = useRef(null);
+	const socialsRef = useRef(null);
+
 	const key = useInputEvent();
 	const { contacts, handleContacts, setContacts } = useContext(ContactsContext);
 	const {
@@ -42,36 +52,149 @@ function Contacts() {
 		}
 	}, [key, contacts, setContacts]);
 
+	useEffect(() => {
+		console.log(contacts);
+
+		const timeline = gsap.timeline({
+			paused: true,
+			defaults: { ease: CustomEase.create('custom', 'M0,0,C0.25,0,0.25,1,1,1') },
+			onStart: () => gsap.set(contactsRef.current, { autoAlpha: 1 }),
+			onReverseComplete: () => gsap.set(contactsRef.current, { autoAlpha: 0 }),
+		});
+
+		timeline.fromTo(
+			contactsRef.current.querySelector('.js-background'),
+			{ scaleY: 0 },
+			{
+				duration: 1,
+				scaleY: 1,
+			},
+		);
+		timeline.fromTo(
+			titleRef.current.querySelector('button'),
+			{ yPercent: 100, opacity: 0 },
+			{ yPercent: 0, opacity: 1, duration: 0.5 },
+			'0.2',
+		);
+		timeline.fromTo(
+			titleRef.current.querySelector('.js-title'),
+			{ yPercent: 100, opacity: 0 },
+			{ yPercent: 0, opacity: 1, duration: 0.5 },
+			'0.3',
+		);
+		timeline.fromTo(
+			phoneRef.current.querySelector('span:nth-child(1)'),
+			{ yPercent: 100, opacity: 0 },
+			{ yPercent: 0, opacity: 1, duration: 0.5 },
+			'0.3',
+		);
+		timeline.fromTo(
+			phoneRef.current.querySelector('span:nth-child(2)'),
+			{ yPercent: 100, opacity: 0 },
+			{ yPercent: 0, opacity: 1, duration: 0.5 },
+			'0.4',
+		);
+		timeline.fromTo(
+			phoneRef.current.querySelector('hr'),
+			{ scaleX: 0, opacity: 0 },
+			{ scaleX: 1, opacity: 1, duration: 0.5 },
+			'0.4',
+		);
+		timeline.fromTo(
+			addressRef.current.querySelector('span:nth-child(1)'),
+			{ yPercent: 100, opacity: 0 },
+			{ yPercent: 0, opacity: 1, duration: 0.5 },
+			'0.4',
+		);
+		timeline.fromTo(
+			addressRef.current.querySelector('span:nth-child(2)'),
+			{ yPercent: 100, opacity: 0 },
+			{ yPercent: 0, opacity: 1, duration: 0.5 },
+			'0.5',
+		);
+		timeline.fromTo(
+			addressRef.current.querySelector('hr'),
+			{ scaleX: 0, opacity: 0 },
+			{ scaleX: 1, opacity: 1, duration: 0.5 },
+			'0.5',
+		);
+		timeline.fromTo(
+			mailRef.current.querySelector('span:nth-child(1)'),
+			{ yPercent: 100, opacity: 0 },
+			{ yPercent: 0, opacity: 1, duration: 0.5 },
+			'0.5',
+		);
+		timeline.fromTo(
+			mailRef.current.querySelector('span:nth-child(2)'),
+			{ yPercent: 100, opacity: 0 },
+			{ yPercent: 0, opacity: 1, duration: 0.5 },
+			'0.6',
+		);
+		timeline.fromTo(
+			mailRef.current.querySelector('hr'),
+			{ scaleX: 0, opacity: 0 },
+			{ scaleX: 1, opacity: 1, duration: 0.5 },
+			'0.7',
+		);
+		timeline.fromTo(
+			socialsRef.current.querySelector('span:nth-child(1)'),
+			{ yPercent: 100, opacity: 0 },
+			{ yPercent: 0, opacity: 1, duration: 0.5 },
+			'0.7',
+		);
+		timeline.fromTo(
+			[...socialsRef.current.querySelectorAll('.js-item')],
+			{ yPercent: 100, opacity: 0 },
+			{ yPercent: 0, opacity: 1, duration: 0.5, stagger: 0.05 },
+			'0.8',
+		);
+
+		timeline.reverse();
+
+		if (contacts) {
+			timeline.play();
+		} else {
+			timeline.reverse(0);
+		}
+	}, [contacts]);
+
 	return (
 		<div
-			className={`Contacts${contacts ? ' is-active' : ''}`}
+			className="Contacts"
 			data-scroll
 			data-scroll-sticky
 			data-scroll-target="#wrapper"
 			ref={contactsRef}>
-			<div className="Contacts__row justify-content-between">
-				<span>Contact informations</span>
+			<div className="Contacts__row justify-content-between" ref={titleRef}>
+				<span className="js-title">Contact informations</span>
 				<button className="Contacts__close" type="button" onClick={handleContacts}>
 					<span>Close</span>
 					<Times />
 				</button>
 			</div>
-			<div className="Contacts__row">
+			<div className="Contacts__row" ref={phoneRef}>
 				<span>Phone</span>
-				{phoneNumber}
+				<span>{phoneNumber}</span>
+				<hr />
 			</div>
-			<div className="Contacts__row">
+			<div className="Contacts__row" ref={addressRef}>
 				<span>Address</span>
-				{address}
+				<span>{address}</span>
+				<hr />
 			</div>
-			<div className="Contacts__row">
+			<div className="Contacts__row" ref={mailRef}>
 				<span>Mail</span>
-				<a href="mailto:{publicEmail}">{publicEmail}</a>
+				<span>
+					<a href="mailto:{publicEmail}">{publicEmail}</a>
+				</span>
+				<hr />
 			</div>
-			<div className="Contacts__row">
+			<div className="Contacts__row" ref={socialsRef}>
 				<span>Social</span>
 				<Socials />
+				<hr />
 			</div>
+			<div className="Contacts__background js-background"></div>
 		</div>
 	);
 }
--- src/components/socials.js
+++ src/components/socials.js
@@ -24,28 +24,28 @@ const Socials = ({ abbr }) => {
 		<div className="Socials">
 			<ul className="Socials__items">
 				{instagram && (
-					<li className="Socials__item">
+					<li className="Socials__item js-item">
 						<a target="_blank" href={instagram} rel="noreferrer noopener">
 							{abbr ? 'Ins.' : 'Instagram'}
 						</a>
 					</li>
 				)}
 				{facebook && (
-					<li className="Socials__item">
+					<li className="Socials__item js-item">
 						<a target="_blank" href={facebook} rel="noreferrer noopener">
 							{abbr ? 'Fb.' : 'Facebook'}
 						</a>
 					</li>
 				)}
 				{linkedin && (
-					<li className="Socials__item">
+					<li className="Socials__item js-item">
 						<a target="_blank" href={linkedin} rel="noreferrer noopener">
 							{abbr ? 'Li.' : 'LinkedIn'}
 						</a>
 					</li>
 				)}
 				{youtube && (
-					<li className="Socials__item">
+					<li className="Socials__item js-item">
 						<a target="_blank" href={youtube} rel="noreferrer noopener">
 							{abbr ? 'Yt.' : 'YouTube'}
 						</a>
