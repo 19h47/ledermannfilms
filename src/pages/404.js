a9c2060363e3af69d538ac64af19d3536b910167
19h47 <jeremylevron@19h47.fr>
2020-10-23 17:44:24 +0200

    :sparkles: 404
    close #16

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
 
 import Layout from '@/components/layout';
+import H0 from '@/components/h0';
 
 export default () => (
-	<Layout>
-		<div className="Site-container">
-			<div className="row">
-				<div className="col-14">
-					<h1>Oops, that's a 404</h1>
+	<Layout className="error404">
+		<div className="Site-container h-100">
+			<div className="row h-100">
+				<div className="col-14 d-flex flex-column align-items-center justify-content-center">
+					<H0 texts={'Erreur 404'} delay={1} />
+					<AniLink
+						className="Button"
+						to="/"
+						data-scroll
+						cover
+						direction="up"
+						bg="#000000">
+						<span>Go back home</span>
+					</AniLink>
 				</div>
 			</div>
+		</div>
+
+		<div className="error404__image" data-scroll>
+			<img src="/404.gif" alt="" />
 		</div>
 	</Layout>
 );
