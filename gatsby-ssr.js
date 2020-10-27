587bfa47bf9d7439265128ea0bdadc21290cee6d
19h47 <jeremylevron@19h47.fr>
2020-10-27 19:18:39 +0100

    :bug: Fix

--- gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ContactsProvider } from './src/context/contacts-context';
-import { ModalProvider } from './src/context/modal-context';
-
-export const wrapRootElement = ({ element }) => (
-	<ContactsProvider>
-		<ModalProvider>{element}</ModalProvider>
-	</ContactsProvider>
-);
