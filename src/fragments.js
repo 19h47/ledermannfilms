3b03f4d7ab6529ed5ae409e9a78534d643420965
19h47 <jeremylevron@19h47.fr>
2022-04-27 11:06:26 +0200

    :bug: fix

--- src/fragments.js
+++ src/fragments.js
@@ -3,9 +3,6 @@ import { graphql } from 'gatsby';
 export const fragments = graphql`
 	fragment HeroImage on File {
 		childImageSharp {
-			fluid(maxWidth: 1502) {
-				...GatsbyImageSharpFluid_withWebp
-			}
 			gatsbyImageData(layout: FIXED)
 		}
 	}
